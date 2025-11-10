022169956588fcf5afb28e6e8365e21270a36e3a
Juskiie <liam@lcb-dev.biz>
2025-11-10 13:01:43 +0000

    Added JavaFX dependency. Added main file.

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,5 +1,7 @@
 plugins {
     id("java")
+    application
+    id("org.openjfx.javafxplugin") version "0.1.0"
 }
 
 group = "casey.lcbdev"
@@ -13,6 +15,11 @@ dependencies {
     testImplementation(platform("org.junit:junit-bom:5.10.0"))
     testImplementation("org.junit.jupiter:junit-jupiter")
     testRuntimeOnly("org.junit.platform:junit-platform-launcher")
+}
+
+javafx {
+    version = "25"
+    modules("javafx.controls", "javafx.fxml")
 }
 
 tasks.test {
